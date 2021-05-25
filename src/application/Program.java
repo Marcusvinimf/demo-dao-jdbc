38e5e2f3c06657b1c3f4e31179fda8c376ea3d62
Marcus <marcus.ferreira@conexasaude.com.br>
2021-05-25 14:56:31 -0300

    Departiment entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "books");
+		
+		System.out.println(obj);
+
+	}
+
+}
