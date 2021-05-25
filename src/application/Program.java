1b8b3f6bfd854613d94a15e694a027cbae82073d
Marcus <marcus.ferreira@conexasaude.com.br>
2021-05-25 15:06:56 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,8 +11,8 @@ public class Program {
 		
 		Department obj = new Department(1, "books");
 		
-		System.out.println(obj);
-
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
-
 }
